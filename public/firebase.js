b257ad3f33345e6629e5697eccf5faf054846268
Haitham <haytham.h.hamdan@gmail.com>
2025-01-08 19:14:49 +0300

    Fix Firebase initialization

--- public/firebase.js
+++ public/firebase.js
@@ -2,13 +2,14 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.16.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/9.16.0/firebase-auth.js";
 
+// Firebase configuration object
 const firebaseConfig = {
-  apiKey: "AIzaSyA1yaz-iAgETeK6CAldsgK_Ep4nhDy5Jag",
-  authDomain: "petroly-auth.firebaseapp.com",
-  projectId: "petroly-auth",
-  storageBucket: "petroly-auth.appspot.com",
-  messagingSenderId: "161063934107",
-  appId: "1:161063934107:web:50c6973e76a5737d2c8e3e",
+  apiKey: "AIzaSyA1yaz-iAgETeK6CAldsgK_Ep4nhDy5Jag",  // Your API Key
+  authDomain: "petroly-auth.firebaseapp.com",       // Your Auth Domain
+  projectId: "petroly-auth",                        // Your Project ID
+  storageBucket: "petroly-auth.appspot.com",        // Your Storage Bucket
+  messagingSenderId: "161063934107",                // Your Sender ID
+  appId: "1:161063934107:web:50c6973e76a5737d2c8e3e",  // Your App ID
 };
 
 // Initialize Firebase
